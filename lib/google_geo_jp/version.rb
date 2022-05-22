fa9392a872f1523f720a4d7494dcbad6b6b03161
Yudai-hub <coffeecoffee@outlook.jp>
2022-05-22 12:58:00 +0900

    first commit

---
+++ lib/google_geo_jp/version.rb
@@ -0,0 +1,3 @@
+module GoogleGeoJp
+  VERSION = "0.1.0"
+end
---
+++ spec/google_geo_jp_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GoogleGeoJp do
+  it "has a version number" do
+    expect(GoogleGeoJp::VERSION).not_to be nil
+  end
+
+  it "geocoding test" do
+    pending
+  end
+end
